53e5029a62236996f6af10aab6534c71b01d26f5
Rishabh <mailrishabh1403@gmail.com>
2017-08-24 23:36:06 +0530

    new files

---
+++ src/components/posts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+
+export default class Posts extends Component {
+  render() {
+    return (
+      <div className="App">
+        Posts
+      </div>
+    );
+  }
+}
+
